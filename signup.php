c32b2f884dfff12dc5a9041dddfc981126153ee0
baralnischal <baralnischal2111@gmail.com>
2013-11-08 16:13:58 -0500

    //
    //

---
+++ signup.php
@@ -0,0 +1,21 @@
+<html>
+<body>
+
+<?php
+$firstname = $_POST["firstname"];
+$lasttname = $_POST["lastname"];
+$newpwd = $_POST["newpwd"];
+$rnewpwd = $_POST["rnewpwd"];
+
+
+$db=mysql_connect  ("localhost", "root",  "slayer123") or die ('I cannot connect to the database because: ' . mysql_error());
+
+$mydb=mysql_select_db("Outnablout");
+
+?>
+
+
+
+
+</body>
+</html>
