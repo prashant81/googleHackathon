007c83df9dc6ea9b015eb9a2059c4a40720bbb6c
baralnischal <baralnischal2111@gmail.com>
2013-11-08 14:00:25 -0500

    all
    files

--- index.php
+++ index.php
@@ -1,11 +1,21 @@
+<!DOCTYPE html>
 <html>
+
+<head>
+<link rel="stylesheet" type="text/css" href="index.css">
+<script src="index.js"></script>
+</head>
+<title></title>
+<body>
+login
+signup
+
+
 
 
 <? php
-
 echo 5;
-
 ?>
 
-
+</body>
 </html>
