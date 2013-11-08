7246da86a3a250a5e4ce28871a1399683ce0a8af
baralnischal <baralnischal2111@gmail.com>
2013-11-08 17:16:46 -0500

    //
    //

--- signup.php
+++ signup.php
@@ -2,14 +2,15 @@
 <body>
 
 <?php
-$firstname = $_POST["firstname"];
-$lasttname = $_POST["lastname"];
+$name = $_POST["name"];
+$email = $_POST["email"];
 $newpwd = $_POST["newpwd"];
 $rnewpwd = $_POST["rnewpwd"];
+$location = $_POST["location"];
+$gender = $_POST["gender"];
 
 
 $db=mysql_connect  ("localhost", "root",  "slayer123") or die ('I cannot connect to the database because: ' . mysql_error());
-<<<<<<< HEAD
 
 $mydb=mysql_select_db("Outnabout");
 =======
