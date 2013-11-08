2e0500c5ced8db3ffae6cef8b04a76d366a66ba5
baralnischal <baralnischal2111@gmail.com>
2013-11-08 16:31:29 -0500

    //
    //

--- signup.php
+++ signup.php
@@ -9,7 +9,6 @@ $rnewpwd = $_POST["rnewpwd"];
 
 
 $db=mysql_connect  ("localhost", "root",  "slayer123") or die ('I cannot connect to the database because: ' . mysql_error());
-
 $mydb=mysql_select_db("Outnablout");
 
 ?>
