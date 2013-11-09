9152ed285055d028e92b385d702035138ee32a73
baralnischal <baralnischal2111@gmail.com>
2013-11-08 21:14:39 -0500

    login.php
    login try code

---
+++ config.inc.php
@@ -0,0 +1,6 @@
+<?php 
+define('DB_HOSTNAME', 'localhost'); 
+define('DB_USERNAME', 'root'); 
+define('DB_PASSWORD', 'slayer123'); 
+define('DB_DATABASE', 'Outnabout'); 
+?> 
--- login.php
+++ login.php
@@ -5,9 +5,46 @@
 $uname = $_POST["uname"];
 $pwd = $_POST["pwd"]; 
 
-$db=mysql_connect  ("localhost", "root",  "slayer123") or die ('I cannot connect to the database because: ' . mysql_error());
+require_once('config.inc.php'); 
+require_once('functions.inc.php'); 
+session_start(); 
+if ($_SESSION['logged_in'] == true) { 
+ redirect('index.html'); 
+} else { 
+ if ( (!isset($uname)) || (!isset($pwd])) 
+OR 
+ (!ctype_alnum($uname)) ) { 
+ redirect('index.html'); 
+ }
 
-$mydb=mysql_select_db("Outnablout");
+ $mysqli = @new mysqli(DB_HOSTNAME, DB_USERNAME, DB_PASSWORD, 
+DB_DATABASE); 
+
+ if (mysqli_connect_errno()) { 
+ printf("Unable to connect to database: %s", 
+mysqli_connect_error()); 
+ exit();
+}
+ 
+ // Escape any unsafe characters before querying database 
+ $username = $mysqli->real_escape_string($uname); 
+ $password = $mysqli->real_escape_string($pwd); 
+ 
+ // Construct SQL statement for query & execute 
+ $sql = "SELECT * FROM User WHERE UserUsername = '" . 
+$username . "' AND UserPassword = '" . $password . "'"; 
+ $result = $mysqli->query($sql); 
+ 
+ // If one row is returned, username and password are valid 
+ if (is_object($result) && $result->num_rows == 1) { 
+ // Set session variable for login status to true 
+ $_SESSION['logged_in'] = true; 
+ redirect('home.html'); //user profile page
+ } else { 
+ redirect('index.html'); 
+ }
+} 
+
 
 ?>
 
