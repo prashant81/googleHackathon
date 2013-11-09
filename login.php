bf01c312a5d2eb571e294dee46bc160e7a0d42d9
baralnischal <baralnischal2111@gmail.com>
2013-11-08 23:44:33 -0500

    //
    //

--- login.php
+++ login.php
@@ -1,40 +1,31 @@
 <?php
+
+
 $uname = $_POST["uname"];
 $pwd = $_POST["pwd"]; 
 
 
 $dbhost = 'localhost';
-   $dbuser = 'root';
-   $dbpass = 'slayer123';
+$dbuser = 'root';
+$dbpass = 'slayer123';
 $con=mysqli_connect($dbhost,$dbuser,$dbpass, "Outnabout");
-// Check connection
+
 if (mysqli_connect_errno())
   {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
   }
-$queryU = 'SELECT UserUsername FROM User WHERE UserUsername = $uname';
-$queryP = 'SELECT UserPassword FROM User WHERE UserPassword = $pwd';
-if ($queryU && $queryP){
-	echo "well done";
-}
-<<<<<<< HEAD
- 
- // Construct SQL statement for query & execute 
- $sql = "SELECT * FROM User WHERE UserUsername = 
-$uname AND UserPassword = $pwd"; 
- $result = $mysqli->query($sql); 
- 
- // If one row is returned, username and password are valid 
- if (is_object($result) && $result->num_rows == 1) { 
- // Set session variable for login status to true 
- $_SESSION['logged_in'] = true; 
- redirect('test.html'); //user profile page
- } else { 
- redirect('index.html'); 
- }
-} 
-=======
 
+//echo "another thing";
+$query = "SELECT * FROM User WHERE (UserUsername='$uname' AND UserPassword = '$pwd')";
+//echo $query;
+$result = mysqli_query($con, $query);
 
->>>>>>> 44a665eba4b0b838c3a6343d3661d22a24ad5812
+while($row = mysqli_fetch_array($result))
+  {
+  echo $row['UserUsername'] . " " . $row['UserPassword'];
+  header("Location: home.html");
+  echo "<br>";
+  }
+
+mysqli_close($con);
 ?>
