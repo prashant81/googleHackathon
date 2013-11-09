44a665eba4b0b838c3a6343d3661d22a24ad5812
baralnischal <baralnischal2111@gmail.com>
2013-11-08 21:53:29 -0500

    login
    login

--- login.php
+++ login.php
@@ -1,5 +1,22 @@
 <?php
 $uname = $_POST["uname"];
 $pwd = $_POST["pwd"]; 
- 
+
+
+$dbhost = 'localhost';
+   $dbuser = 'root';
+   $dbpass = 'slayer123';
+$con=mysqli_connect($dbhost,$dbuser,$dbpass, "Outnabout");
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+$queryU = 'SELECT UserUsername FROM User WHERE UserUsername = $uname';
+$queryP = 'SELECT UserPassword FROM User WHERE UserPassword = $pwd';
+if ($queryU && $queryP){
+	echo "well done";
+}
+
+
 ?>
